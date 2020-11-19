a4ca806e799bec10382221d084c6596f077eac2d
migueldewoutersufv <miguel.dewouters@gmail.com>
2020-11-19 20:44:00 +0100

    codificado una primera version de golfPlayer.java inicializado EscribeJSON

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -1,11 +1,12 @@
 //Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
-package ...
-imports ...
+package org.dis;
+import com.google.code.gson;
+import com.opencsv;
 
 public class EscribeJSON {
 
     public static void main(String[] args) {
-    ....
+    int a =0;
     }
     
  }
--- src/main/java/org.dis/golfPlayer.java
+++ src/main/java/org.dis/golfPlayer.java
@@ -1,1 +1,23 @@
-//Clase para crear el objeto plano (POJO) con anotaciones
+package org.dis;
+public class golfPlayer {
+
+    private int posicion;
+    private String nombre;
+    private String apellido;
+    private String pais;
+    private int edad;
+
+
+    public golfPlayer(int posicion, String nombre, String apellido, String pais, int edad) {
+        this.posicion = posicion;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.pais = pais;
+    }
+
+    @Override
+    public String toString() {
+        return "Posicion=" + posicion + ", Nombre=" + nombre + ", Apellido=" + apellido + ", Pais=" + pais;
+    }
+
+}
