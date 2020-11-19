546aa5124290c6d109704176bb13682ce365e7ec
migueldewoutersufv <miguel.dewouters@gmail.com>
2020-11-19 21:14:51 +0100

    agnadido @CSVBindByName a golfPlayer.java

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -1,12 +1,13 @@
 //Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
 package org.dis;
-import com.google.code.gson;
-import com.opencsv;
+
+import com.google.gson.Gson;
+import com.opencsv.CSVReader;
 
 public class EscribeJSON {
 
     public static void main(String[] args) {
     int a =0;
     }
-    
+    BufferedReader fileReader = new BufferedReader(new FileReader("path a mi fichero csv"));
  }
