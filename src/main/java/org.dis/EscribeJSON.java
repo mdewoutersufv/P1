ab1fc671ac77f90b57cf59fe93f0c0ca895b5d48
migueldewoutersufv <miguel.dewouters@gmail.com>
2020-11-19 21:36:12 +0100

    commit final

--- src/main/java/org.dis/EscribeJSON.java
+++ src/main/java/org.dis/EscribeJSON.java
@@ -1,12 +1,12 @@
 package org.dis;//Fichero para mostrar por consola el JSON generado a partir del CSV y almacenarlo en el fichero best-golf-players.json
 
 import com.google.gson.Gson;
-import com.opencsv;
+
+import java.util.List;
 
 public class EscribeJSON {
 
     public static void main(String[] args) {
-    int a =0;
+        List<golfPlayer> players = parseaCSV("best-golf-players.csv");
     }
-    BufferedReader fileReader = new BufferedReader(new FileReader("path a mi fichero csv"));
  }
--- src/main/java/org.dis/LeeCSV.java
+++ src/main/java/org.dis/LeeCSV.java
@@ -5,17 +5,17 @@ import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
 
 
 public class LeerCSV {
 
-    public List<golfPlayer> parseaCSV(String path) {
-        BufferedReader fileReader = new BufferedReader(new FileReader("best-golf-players.csv"));
+    public List<golfPlayer> parseaCSV(String path) throws FileNotFoundException {
+        BufferedReader fileReader = new BufferedReader(new FileReader(path));
         CsvToBean csvToBean = new CsvToBeanBuilder(fileReader).withType(golfPlayer.class).withIgnoreLeadingWhiteSpace(true).build();
-        List<golfPlayer> players;
-        players = CsvToBean.parse();
+        List<golfPlayer> players = CsvToBean.parse();
         return players;
     }
 }
